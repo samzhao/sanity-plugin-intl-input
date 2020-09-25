54df2405ff870317e1eaf4e2d636366acb9431bb
samzhao <zhao6518@gmail.com>
2020-09-25 09:16:20 -0700

    Change docId format from [prefix].[id].[lang] to [prefix]-[id]-[lang]

--- src/actions/PublishWithi18nAction.ts
+++ src/actions/PublishWithi18nAction.ts
@@ -1,8 +1,12 @@
-import * as React from 'react';
-import moment, { lang } from 'moment';
-import { SanityDocument, Patch } from '@sanity/client';
-import { IResolverProps, Ti18nSchema, IUseDocumentOperationResult } from '../types';
-import { useDocumentOperation, useSyncState } from '@sanity/react-hooks';
+import * as React from "react";
+import moment, { lang } from "moment";
+import { SanityDocument, Patch } from "@sanity/client";
+import {
+  IResolverProps,
+  Ti18nSchema,
+  IUseDocumentOperationResult,
+} from "../types";
+import { useDocumentOperation, useSyncState } from "@sanity/react-hooks";
 import {
   getSchema,
   getLanguagesFromOption,
@@ -12,14 +16,17 @@ import {
   getSanityClient,
   getConfig,
   buildDocId,
-} from '../utils';
+} from "../utils";
 
 export const PublishWithi18nAction = (props: IResolverProps) => {
   const schema: Ti18nSchema = getSchema(props.type);
   const config = getConfig(schema);
   const baseDocumentId = getBaseIdFromId(props.id);
   const syncState = useSyncState(props.id, props.type);
-  const { publish } = useDocumentOperation(props.id, props.type) as IUseDocumentOperationResult;
+  const { publish } = useDocumentOperation(
+    props.id,
+    props.type
+  ) as IUseDocumentOperationResult;
   const [publishing, setPublishing] = React.useState(false);
 
   React.useEffect(() => {
@@ -27,48 +34,62 @@ export const PublishWithi18nAction = (props: IResolverProps) => {
   }, [publish.disabled, props.draft, syncState.isSyncing]);
 
   return {
-    disabled:
-      publishing
-      || publish.disabled
-      || syncState.isSyncing,
-    label: publishing
-      ? config.messages?.publishing
-      : config.messages?.publish,
+    disabled: publishing || publish.disabled || syncState.isSyncing,
+    label: publishing ? config.messages?.publishing : config.messages?.publish,
     onHandle: async () => {
       setPublishing(true);
       const client = getSanityClient();
       const fieldName = config.fieldNames.lang;
       const refsFieldName = config.fieldNames.references;
       const langs = await getLanguagesFromOption(config.languages);
-      const languageId = getLanguageFromId(props.id) || getBaseLanguage(langs, config.base)?.name;
+      const languageId =
+        getLanguageFromId(props.id) ||
+        getBaseLanguage(langs, config.base)?.name;
 
-      await client.createIfNotExists({ _id: props.id, _type: props.type, _createdAt: moment().utc().toISOString() });
-      await client.patch(props.draft?._id || props.id, { set: { [fieldName]: languageId } }).commit();
+      await client.createIfNotExists({
+        _id: props.id,
+        _type: props.type,
+        _createdAt: moment().utc().toISOString(),
+      });
+      await client
+        .patch(props.draft?._id || props.id, {
+          set: { [fieldName]: languageId },
+        })
+        .commit();
       publish.execute();
 
-      const translatedDocuments = await client.fetch<SanityDocument[]>('*[_id in path($path)]', {
-        path: buildDocId(baseDocumentId, '*')
-      });
+      const translatedDocuments = await client.fetch<SanityDocument[]>(
+        "*[_id match path($id)]",
+        {
+          id: buildDocId(baseDocumentId, "**"),
+        }
+      );
       if (translatedDocuments.length > 0) {
-        await client.createIfNotExists({ _id: baseDocumentId, _type: props.type, _createdAt: moment().utc().toISOString() });
-        await client.patch(baseDocumentId, {
-          set: {
-            [refsFieldName]: translatedDocuments.map((doc) => {
-              const lang = getLanguageFromId(doc._id);
-              return {
-                _key: doc._id,
-                lang,
-                ref: {
-                  _type: 'reference',
-                  _ref: doc._id,
-                }
-              };
-            }, {})
-          }
-        }).commit();
+        await client.createIfNotExists({
+          _id: baseDocumentId,
+          _type: props.type,
+          _createdAt: moment().utc().toISOString(),
+        });
+        await client
+          .patch(baseDocumentId, {
+            set: {
+              [refsFieldName]: translatedDocuments.map((doc) => {
+                const lang = getLanguageFromId(doc._id);
+                return {
+                  _key: doc._id,
+                  lang,
+                  ref: {
+                    _type: "reference",
+                    _ref: doc._id,
+                  },
+                };
+              }, {}),
+            },
+          })
+          .commit();
       }
 
       props.onComplete && props.onComplete();
-    }
+    },
   };
-}
+};
--- src/structure/index.tsx
+++ src/structure/index.tsx
@@ -1,42 +1,49 @@
-import 'regenerator-runtime';
-import { StructureBuilder as S } from '@sanity/structure';
-import { IDefaultDocumentNodeStructureProps } from './IDefaultDocumentNodeStructureProps';
-import { Ti18nSchema } from '../types';
-import { ListItemBuilder } from '@sanity/structure/lib/ListItem';
-import { TranslationsComponentFactory } from './TranslationsComponentFactory';
-import { getSchema, getConfig } from '../utils';
-import { SchemaType } from '@sanity/structure/lib/parts/Schema';
-import { MaintenanceTab } from './MaintenanceTab';
-import { I18nPrefix } from '../constants';
+import "regenerator-runtime";
+import { StructureBuilder as S } from "@sanity/structure";
+import { IDefaultDocumentNodeStructureProps } from "./IDefaultDocumentNodeStructureProps";
+import { Ti18nSchema } from "../types";
+import { ListItemBuilder } from "@sanity/structure/lib/ListItem";
+import { TranslationsComponentFactory } from "./TranslationsComponentFactory";
+import { getSchema, getConfig } from "../utils";
+import { SchemaType } from "@sanity/structure/lib/parts/Schema";
+import { MaintenanceTab } from "./MaintenanceTab";
+import { I18nPrefix } from "../constants";
 
 const hasIcon = (schemaType?: SchemaType | string): boolean => {
-  if (!schemaType || typeof schemaType === 'string') {
-    return false
+  if (!schemaType || typeof schemaType === "string") {
+    return false;
   }
-  return Boolean(schemaType.icon)
-}
+  return Boolean(schemaType.icon);
+};
 
 export const getDocumentNodeViewsForSchemaType = (type: string) => {
   const schema: Ti18nSchema = getSchema(type);
   return [
     S.view.form(),
-    S.view.component(TranslationsComponentFactory(schema)).title('Translations')
+    S.view
+      .component(TranslationsComponentFactory(schema))
+      .title("Translations"),
   ];
-}
+};
 
-export const getDefaultDocumentNode = (props: IDefaultDocumentNodeStructureProps) => {
+export const getDefaultDocumentNode = (
+  props: IDefaultDocumentNodeStructureProps
+) => {
   const schema: Ti18nSchema = getSchema(props.schemaType);
   if (schema && schema.i18n) {
-    return S.document().views(getDocumentNodeViewsForSchemaType(props.schemaType));
+    return S.document().views(
+      getDocumentNodeViewsForSchemaType(props.schemaType)
+    );
   }
   return S.document();
 };
 
 export const getDocumentTypes = () => {
   const listItemsWithouti18n: ListItemBuilder[] = [];
-  const listItemsWithi18n = S.documentTypeListItems().filter(l => {
+  const listItemsWithi18n = S.documentTypeListItems().filter((l) => {
     const schemaType = l.getSchemaType();
-    const hasi18n = schemaType && typeof schemaType !== 'string' && (schemaType as any).i18n;
+    const hasi18n =
+      schemaType && typeof schemaType !== "string" && (schemaType as any).i18n;
     if (!hasi18n) listItemsWithouti18n.push(l);
     return hasi18n;
   });
@@ -49,16 +56,16 @@ export const getDocumentTypes = () => {
 export const getMaintenanceTabComponent = () => {
   const config = getConfig();
   return S.component(MaintenanceTab)
-    .title(config.messages?.translationsMaintenance?.title || '')
+    .title(config.messages?.translationsMaintenance?.title || "")
     .id(`__i18n_translations_maintenance_tab`);
-}
+};
 
 export const getMaintenanceListItem = () => {
   const config = getConfig();
   return S.listItem()
-  .id(`__i18n_translations_maintenance_tab`)
-  .title(config.messages?.translationsMaintenance?.title || '')
-  .child(getMaintenanceTabComponent());
+    .id(`__i18n_translations_maintenance_tab`)
+    .title(config.messages?.translationsMaintenance?.title || "")
+    .child(getMaintenanceTabComponent());
 };
 
 export const getFilteredDocumentTypeListItems = () => {
@@ -67,19 +74,21 @@ export const getFilteredDocumentTypeListItems = () => {
 
   const items = [
     ...types.withoutI18n,
-    ...types.withI18n.map(l => (
+    ...types.withI18n.map((l) =>
       l.child(
         S.documentList()
-          .id(l.getId() || '')
-          .title(l.getTitle() || '')
-          .filter('!(_id in path($path)) && !(_id in path($drafts)) && _type == $type')
+          .id(l.getId() || "")
+          .title(l.getTitle() || "")
+          .filter(
+            "!(_id match path($id)) && !(_id match path($drafts)) && _type == $type"
+          )
           .params({
-            path: `${I18nPrefix}.**`,
-            drafts: `drafts.${I18nPrefix}.**`,
-            type: l.getId()
+            id: `${I18nPrefix}-**`,
+            drafts: `drafts.${I18nPrefix}-**`,
+            type: l.getId(),
           })
       )
-    )),
+    ),
   ];
 
   if (config.withTranslationsMaintenance) {
@@ -95,8 +104,8 @@ export default () => {
 
   const items = getFilteredDocumentTypeListItems();
   return S.list()
-    .id('__root__')
-    .title('Content')
+    .id("__root__")
+    .title("Content")
     .items(items)
-    .showIcons(items.some(item => hasIcon(item.getSchemaType())));
-}
+    .showIcons(items.some((item) => hasIcon(item.getSchemaType())));
+};
